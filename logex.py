d6118ae8930f4ca828e5ddd259553c9f93ab0557
Jim Bob Pipes <jimbob@depot6.com>
2012-03-14 13:54:57 -0600

    Change to scan all logs for all station records and output new Db for each station

--- logex.py
+++ logex.py
@@ -1,51 +1,49 @@
+# scan all log files for given day for all valid stations
+# sort by actual play time
+# replace played CUT with rotator CUT if applicable
+# consolidate all log entries into new station specific db
+# save each new db locally
+
 import os
 import glob
+from collections import defaultdict
 from dbfpy import dbf
 
-stations = ("KMTI", "KLGL", "KMGR", "KSVC", "KWUT")
 day = ''
 month = ''
 year = ''
-log = []
-print "Which station do you want to extract logs for?"
-print "Please select a number:"
-print "1: KMTI"
-print "2: KLGL"
-print "3: KMGR"
-print "4: KSVC"
-print "5: KWUT"
-print
+valid_stations = ['KMTI', 'KLGL', 'KMGR', 'KWUT', 'KSVC']
+log = defaultdict(list)
+print "What date do you want to extract logs for?"
+print "Enter the date as: mm.dd.yyyy"
 selection = raw_input("> ")
-number = int(selection)
-if number in range(1, 6):
-    station = stations[number - 1]
-    
-    print "What date do you want to extract logs for?"
-    print "Enter the date as: mm.dd.yyyy"
-    selection = raw_input("> ")
-    date = selection.split(".")
-    month = date[0]
-    day = date[1]
-    year = date[2]
-    date = month + day + year[2:4]
+date = selection.split(".")
+month = date[0]
+day = date[1]
+year = date[2]
+date = month + day + year[2:4]
 
-    # open all files that match the specified date and extract all log entries 
-    # for the given station
-    # F:\DAD\
-    for filename in glob.glob(os.path.normpath('F:/DAD/Asplay/*%s.DBF' % date)):
-        db = dbf.Dbf(filename)
-        # print db
-        for record in db:
-        	if record['PLAYLIST'] == month + day + station:
-        		log.append(record)
-        db.close()
+# open all files that match the specified date and extract all log entries
+# for filename in glob.glob(os.path.normpath('./db/*%s.DBF' % date)):
+for filename in glob.glob(os.path.normpath('F:/DAD/Asplay/*%s.DBF' % date)):
+    db = dbf.Dbf(filename)
+    # print db
+    for record in db:
+        station = record['PLAYLIST'].lstrip(month + day)
+        if station in valid_stations:
+            log[station].append(record)
+    db.close()
 
-    if len(log) > 0:
+
+if len(log) > 0:
+    # print len(log)
+    # print log.keys()
+
+    for station in log:
         # sort log entries by actual play time
-        log = sorted(log, key=lambda record: record[8])
-        # print len(log)
-
+        log[station] = sorted(log[station], key=lambda record: record[8])
         # Create new db and setup record fields
+        # db = dbf.Dbf(os.path.normpath('./asplay/%s%s.DBF' % (station, date)), new=True)
         db = dbf.Dbf(os.path.normpath('C:/IMPORT/asplay/%s%s.DBF' % (station, date)), new=True)
         db.addField(
             ("CUT", "C", 5),
@@ -72,7 +70,7 @@ if number in range(1, 6):
         # print db
 
         # loop through log entries and insert into ne db
-        for record in log:
+        for record in log[station]:
             rec = db.newRecord()
             rec["STATUS"] = record[1]
             rec["TITLE"] = record[2]
@@ -93,7 +91,7 @@ if number in range(1, 6):
             rec["DEVICEID"] = record[17]
             rec["GROUP"] = record[18]
             rec["WASPLAYED"] = record[19]
-            # If cut is played from a rotator then log the rotator cut number 
+            # If cut is played from a rotator then log the rotator cut number
             # instead of the played cut
             if record[13].startswith('#R'):
                 comment = record[13].split()
@@ -104,15 +102,7 @@ if number in range(1, 6):
             rec.store()
         db.close()
         # print db
-        print "Finished! %s records were extracted for %s on %s/%s/%s" % (len(log), station, month, day, year)
-    else:
-        print "There are no log entries for %s on %s/%s/%s" % (station, month, day, year)
+        print "Finished with %s... %s records extracted" % (station, len(log[station]))
+    print "Log extraction is complete for %s/%s/%s" % (month, day, year)
 else:
-    print "Sorry! That isn't one of the options."
-    exit
-    
-# scan all logs for given day for specified station
-# sort by actual play time
-# replace played CUT with rotator CUT if applicable
-# consolidate all log entries into new db
-# save new db locally
+    print "There are no log entries for %s/%s/%s" % (month, day, year)
